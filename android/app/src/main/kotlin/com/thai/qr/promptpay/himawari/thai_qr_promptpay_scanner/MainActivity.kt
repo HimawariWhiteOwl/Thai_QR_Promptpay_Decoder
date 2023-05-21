7cce5e389e53870ff0aceec1aafbfe765f039430
Himawari <vt.ho.himawari@gmail.com>
2023-05-22 01:36:36 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/thai/qr/promptpay/himawari/thai_qr_promptpay_scanner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thai.qr.promptpay.himawari.thai_qr_promptpay_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:thai_qr_promptpay_scanner/src/home/home_page.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Promptpay Decoder",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      // debugShowCheckedModeBanner: false,
+    );
+  }
+}
